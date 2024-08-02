d57d80cd9aeb947f3b4d692638d2ea08ec9632cd
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-03 01:11:08 +0300

    add docs and overload for "getInstanceFromPath"

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import type { Index, Paths } from "./Shared";
+import type { Index, Paths, TryIndex } from "./Shared";
 
 export * from "./Manifest";
 export * from "./Shared";
@@ -6,11 +6,27 @@ export * from "./Util";
 export * from "./AtomicBinding";
 export * from "./AtomicBindingConfiguration";
 
+/**
+ * This function statically indexes the Instance for the provided `Path`.
+ */
 export function getInstanceFromPath<
 	const Root extends Instance = Instance,
 	const Path extends Paths<Root> = Paths<Root>,
 >(root: Root, path: Path): Index<Root, Path> | undefined;
 
+/**
+ * This function accepts arbitrary `Path` string.
+ * The type of the instance that `Path` is referring to will be attempted to indexed statically.
+ * Otherwise `undefined` type will be returned.
+ */
+export function getInstanceFromPath<
+	const Root extends Instance = Instance,
+	const TryPath extends string = string,
+>(root: Root, tryPath: TryPath): TryIndex<Root, TryPath> | undefined;
+
+/**
+ * This function utilizes `WaitForChild` method of the instance to recursively await the requested instance.
+ */
 export function waitForInstanceFromPath<
 	const Root extends Instance = Instance,
 	const Path extends Paths<Root> = Paths<Root>,
