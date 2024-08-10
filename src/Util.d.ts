d4b34f3cea60a977242df1123c0d2d12c3bdf9f4
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-10 22:05:34 +0300

    add "ExtractDescendants"  and "InferDescendant" types

--- src/Util.d.ts
+++ src/Util.d.ts
@@ -3,13 +3,12 @@ export type EmptyObject = Record<string, never>;
 /**
  * Checks whether provided type has children.
  */
-export type HasChildren<T extends Instance> =
-	ExtractChildren<T> extends EmptyObject ? true : false;
+export type HasChildren<T> = ExtractChildren<T> extends EmptyObject ? true : false;
 
 /**
  * Omits all of the keys that do not represent the child Instance.
  */
-export type ExtractChildren<Root extends Instance> = {
+export type ExtractChildren<Root> = {
 	[Key in keyof Root as Key extends string
 		? Root[Key] extends Instance
 			? // Weird trick to exclude `never` type
@@ -19,3 +18,19 @@ export type ExtractChildren<Root extends Instance> = {
 			: never
 		: never]: Root[Key];
 };
+
+/**
+ * Should be used alongside {@link InferDescendant} type to get the type of nested Instance.
+ */
+export type ExtractDescendants<Root> = {
+	[Key in keyof Root as Key extends string
+		? Root[Key] extends Instance
+			? // Weird trick to exclude `never` type
+				[Root[Key]] extends [never]
+				? never
+				: Key
+			: never
+		: never]: ExtractDescendants<Root[Key]>;
+};
+
+export type InferDescendant<T> = T extends ExtractDescendants<infer A> ? A : never;
