10e9bddd64d68cec5a3958f2d8a328db5cc7ce18
nonamie <53796817+mathfox@users.noreply.github.com>
2024-08-10 22:01:01 +0300

    refactor types for helper functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
-import type { Index, Paths, TryIndex } from "./Shared";
+import type { Index, TryIndex } from "./Shared";
 
 export * from "./Manifest";
 export * from "./Shared";
@@ -7,27 +7,20 @@ export * from "./AtomicBinding";
 export * from "./AtomicBindingConfiguration";
 
 /**
- * This function statically indexes the Instance for the provided `Path`.
- */
-export function getInstanceFromPath<
-	const Root extends Instance = Instance,
-	const Path extends Paths<Root> = Paths<Root>,
->(root: Root, path: Path): Index<Root, Path> | undefined;
-
-/**
  * This function accepts arbitrary `Path` string.
+ *
  * The type of the instance that `Path` is referring to will be attempted to indexed statically.
  * Otherwise `undefined` type will be returned.
  */
-export function getInstanceFromPath<
-	const Root extends Instance = Instance,
-	const TryPath extends string = string,
->(root: Root, tryPath: TryPath): TryIndex<Root, TryPath> | undefined;
+export function getInstanceFromPath<const Root extends Instance = Instance, const TryPath extends string = string>(
+	root: Root,
+	tryPath: TryPath,
+): TryIndex<Root, TryPath> | undefined;
 
 /**
  * This function utilizes `WaitForChild` method of the instance to recursively await the requested instance.
  */
-export function waitForInstanceFromPath<
-	const Root extends Instance = Instance,
-	const Path extends Paths<Root> = Paths<Root>,
->(root: Root, path: Path): Index<Root, Path>;
+export function waitForInstanceFromPath<const Root extends Instance = Instance, const Path extends string = string>(
+	root: Root,
+	path: Path,
+): Index<Root, Path>;
