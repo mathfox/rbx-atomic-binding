9d748de2927eb3fa3347a955459ab4f6688f99fb
nonamie <53796817+mathfox@users.noreply.github.com>
2024-09-02 19:43:38 +0300

    refactors

--- src/Util.d.ts
+++ src/Util.d.ts
@@ -1,38 +1,46 @@
+/**
+ * Prettifies a type for better readability.
+ *
+ * Hint: This type has no effect and is only used so that TypeScript displays
+ * the final type in the preview instead of the utility types used.
+ */
+export type Prettify<TObject> = { [TKey in keyof TObject]: TObject[TKey] } & {};
+
 export type EmptyObject = Record<string, never>;
 
 /**
  * Checks whether provided type has children.
  */
-export type HasChildren<T> = ExtractChildren<T> extends EmptyObject
+export type HasChildren<TValue> = ExtractChildren<TValue> extends EmptyObject
 	? true
 	: false;
 
 /**
  * Omits all of the keys that do not represent the child Instance.
  */
-export type ExtractChildren<Root> = {
-	[Key in keyof Root as Key extends string
-		? Root[Key] extends Instance
+export type ExtractChildren<TRoot> = {
+	[Key in keyof TRoot as Key extends string
+		? TRoot[Key] extends Instance
 			? // Weird trick to exclude `never` type
-				[Root[Key]] extends [never]
+				[TRoot[Key]] extends [never]
 				? never
 				: Key
 			: never
-		: never]: Root[Key];
+		: never]: TRoot[Key];
 };
 
 /**
  * Should be used alongside {@link InferDescendant} type to get the type of nested Instance.
  */
-export type ExtractDescendants<Root> = {
-	[Key in keyof Root as Key extends string
-		? Root[Key] extends Instance
+export type ExtractDescendants<TRoot> = {
+	[Key in keyof TRoot as Key extends string
+		? TRoot[Key] extends Instance
 			? // Weird trick to exclude `never` type
-				[Root[Key]] extends [never]
+				[TRoot[Key]] extends [never]
 				? never
 				: Key
 			: never
-		: never]: ExtractDescendants<Root[Key]>;
+		: never]: ExtractDescendants<TRoot[Key]>;
 };
 
 export type InferDescendant<T> = T extends ExtractDescendants<infer A>
--- src/index.d.ts
+++ src/index.d.ts
@@ -7,10 +7,19 @@ export * from "./AtomicBinding";
 export * from "./AtomicBindingConfiguration";
 
 /**
+ * Parsed the provided `path` into an array of strings.
+ * This function also excludes any empty string entries from the result array.
+ * That means that the values `child_1/child_2` and `child_1/child_2/` will have exactly the same output.
+ */
+export function parsePath(path: string): string;
+
+/**
  * This function accepts arbitrary `Path` string.
  *
  * The type of the instance that `Path` is referring to will be attempted to indexed statically.
  * Otherwise `undefined` type will be returned.
+ *
+ * This function normalized the provided path using {@link parsePath} function.
  */
 export function getInstanceFromPath<
 	const Root extends Instance = Instance,
@@ -19,6 +28,8 @@ export function getInstanceFromPath<
 
 /**
  * This function utilizes `WaitForChild` method of the instance to recursively await the requested instance.
+ *
+ * This function normalized the provided path using {@link parsePath} function.
  */
 export function waitForInstanceFromPath<
 	const Root extends Instance = Instance,
